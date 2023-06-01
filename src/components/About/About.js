041f9aa5f82206aae65de875458b61ef8aef72b6
aborgono <albertoiborgonovo@gmail.com>
2023-06-01 16:48:17 -0400

    added border and content to about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,8 +3,15 @@ import './About.scss';
 function About() {
     return (
     <>
-    <h1>About</h1>
-    <p></p>
+        <section className='about__me'>
+            <h1 className='about__me__header'>About</h1>
+            <p className='about__me__paragraph'>With a degree in finance and having recently completed a full-stack software engineering bootcamp, I have developed a diverse skill set that allows me to pursue exciting career opportunities. Combining my knowledge of finance and software engineering, I can explore various paths in the technology industry.</p>
+            <p className='about__me__paragraph'>My proficiency in JavaScript, React, Node.js, and other programming languages enables me to develop web applications, mobile apps, and backend systems across different sectors. I can contribute to creating user-friendly interfaces, implementing complex functionality, and ensuring smooth data flow between frontend and backend components.</p>
+            <p className='about__me__paragraph'>What sets me apart is my background in finance, which adds a unique perspective to my software engineering skills. I can bring a data-driven approach to my work by incorporating financial analytics, forecasting models, or risk assessment algorithms into various applications. This can be particularly valuable in sectors such as retail, healthcare, or logistics, where financial insights can optimize operations and drive decision-making.</p>
+            <p className='about__me__paragraph'>Moreover, my combined knowledge of finance and software engineering makes me a valuable asset in the fintech industry. By joining fintech startups or established companies, I can contribute to developing innovative digital payment solutions, blockchain applications, or robo-advisory platforms that revolutionize the way people manage their finances.</p>
+            <p className='about__me__paragraph'>Beyond development roles, I can also leverage my expertise in both domains to pursue positions such as technology consultant, project manager, or technical product manager. In these roles, I can utilize my skills to bridge the gap between business requirements and technical implementation, ensuring successful project delivery and driving the growth of impactful products.</p>
+            <p className='about__me__paragraph'>In summary, with my degree in finance and completion of a software engineering bootcamp, I have the versatility to pursue a wide range of career opportunities. I can apply my programming skills to different sectors, leverage my financial knowledge to add value to software development projects, and contribute to the creation of innovative technology solutions.</p>
+        </section>
     </>
 );
 }
